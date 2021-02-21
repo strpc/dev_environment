60ed66d7600badd97662263d99e473f5b9a55333
alexey <terr.kuzyashin@gmail.com>
2021-02-21 15:20:03 +0300

    Aliases

---
+++ .dev_aliases.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+alias dev_start_clickhouse="@install_path/make clickhouse"
+alias dev_stop_clickhouse="@install_path/make clickhouse_down"
+
+alias dev_start_mongodb="@install_path/make mongodb"
+alias dev_stop_mongodb="@install_path/make mongodb_down"
+
+alias dev_start_mysql="@install_path/make mysql"
+alias dev_stop_mysql="@install_path/make mysql_down"
+alias dev_mysql_query_log="@install_path/make mysql_query_log"
+
+alias dev_start_postgres="@install_path/make postgres"
+alias dev_stop_postgres="@install_path/make postgres_down"
+
+alias dev_start_redis_cluster="@install_path/make redis_cluster"
+alias dev_stop_redis_cluster="@install_path/make redis_cluster_down"
+
+alias dev_start_redis="@install_path/make redis"
+alias dev_stop_redis="@install_path/make redis_down"
+
+alias dev_start_rabbitmq="@install_path/make rabbitmq"
+alias dev_stop_rabbitmq="@install_path/make rabbitmq_down"
+
+alias dev_start_ftp="@install_path/make ftp"
+alias dev_stop_ftp="@install_path/make ftp_down"
+
+alias dev_start_nats="@install_path/make nats"
+alias dev_stop_nats="@install_path/make nats_down"
+
+alias dev_start_nats_cluster="@install_path/make nats_cluster"
+alias dev_stop_nats_cluster="@install_path/make nats_cluster_down"
+
+alias dev_start_kafka="@install_path/make kafka"
+alias dev_stop_kafka="@install_path/make kafka_down"
---
+++ install.sh
@@ -0,0 +1,64 @@
+DEFAULT_INSTALL_PATH="$HOME"
+
+Help()
+{
+   # Display Help
+   echo "Script to clone git repo and create aliases."
+   echo
+   echo "Syntax: InstallScript [-p|h]"
+   echo "options:"
+   echo "p     Specify install path (Default: $DEFAULT_INSTALL_PATH)"
+   echo "h     Print this Help."
+   echo
+}
+
+
+while getopts p:h flag
+do
+    case "${flag}" in
+        p) # read install path
+          install_path=${OPTARG};;
+        h) # display Help
+         Help
+         exit;;
+       \?) # incorrect option
+         echo "Error: Invalid option"
+         exit;;
+    esac
+done
+
+
+if [ -n "$install_path" ]
+then
+  echo "Will use ${install_path}"
+else
+  install_path="${DEFAULT_INSTALL_PATH}"
+  echo "Will use ${install_path}"
+fi
+
+if [ -d "${install_path}/dev_environment" ]
+then
+  rm -rf "${install_path}"/dev_environment
+fi
+git clone https://github.com/Kuzyashin/dev_environment.git "${install_path}/dev_environment"
+cd "${install_path}"/dev_environment || exit
+cp .env.example .env
+sed "s~@install_path~${install_path}~g" .dev_aliases.sh > "${install_path}"/.dev_aliases.sh
+
+if [ -n "$ZSH_VERSION" ]
+then
+   echo "Will create aliases for ZSH"
+   FILE="${HOME}/.zshrc"
+   echo "SOURCE ${install_path}/.dev_aliases.sh" >> FILE
+   echo "Aliases created"
+elif [ -n "$BASH_VERSION" ]
+then
+   echo "Will create aliases for BASH"
+   FILE="${HOME}/.bashrc"
+   echo "SOURCE ${install_path}/.dev_aliases.sh" >> FILE
+   echo "Aliases created"
+else
+   echo "Cannot install in auto mode. PLZ mke aliases yourself"
+   exit;
+fi
+
